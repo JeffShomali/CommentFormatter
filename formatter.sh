91eccbb37ef3cbc68ae079bf62bf20fe6e9a2b2c
Jeff <jeffshomali@gmail.com>
2015-06-03 11:55:07 -0700

    Initiate formatter.sh file

---
+++ formatter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE: formatter.sh
+#
+#         USAGE: ./formatter.sh filename
+#
+#   DESCRIPTION: Formating Assembli source comment
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Jeff, Shomali
+#  ORGANIZATION: Jeffshomali.com
+#       CREATED: Wednesday Jun 03 2015 21:20
+#      REVISION:  ---
+#===============================================================================
